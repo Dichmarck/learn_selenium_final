5e0c7edddf298331d17c24a8c8f058ca09474c84
Danila Budnov <danchikosik@gmail.com>
2022-07-19 18:53:57 +0700

    Modified 'should_be_login_link' func in main_page.py with assert. Cathed NoSuchElementException in method 'is_element_present' of BasePage class

--- pages/base_page.py
+++ pages/base_page.py
@@ -1,7 +1,23 @@
+from selenium.common import NoSuchElementException
+from selenium.webdriver.common.by import By
+
+
 class BasePage:
-    def __int__(self, browser, url):
+    def __init__(self, browser, url, timeout=10):
         self.browser = browser
         self.url = url
+        self.browser.implicitly_wait(timeout)
 
     def open(self):
         self.browser.get(self.url)
+
+    def is_elemet_present(self, how, what):
+        try:
+            self.browser.find_element(how, what)
+        except NoSuchElementException:
+            return False
+        return True
+
+
+
+
--- pages/main_page.py
+++ pages/main_page.py
@@ -1,9 +1,15 @@
+import pytest
 from selenium.webdriver.common.by import By
 from .base_page import BasePage
 
 
 class MainPage(BasePage):
+
+    def should_be_login_link(self):  # заранее провальный
+        assert self.is_elemet_present(By.CSS_SELECTOR, "#login_link")
+
     def go_to_login_page(self):
         login_link = self.browser.find_element(By.CSS_SELECTOR, "#login_link")
         login_link.click()
+
 
